0cfb90a85ccb7f7cea7764051192d366ed50de3e
Harris <tc.harris@ubc.ca>
2026-05-08 14:12:57 -0700

    updated file path

--- tests/testthat/test-transition.R
+++ tests/testthat/test-transition.R
@@ -6,7 +6,7 @@ library(testthat)
 if (!dir.exists(file.path(.projRoot, "modules"))) {
   .projRoot <- getwd()
 }
-source(file.path(.projRoot, "modules", "snagDecay", "R", "transition.R"))
+source(file.path(.projRoot, "modules", "DeadWood_snagDecay", "R", "transition.R"))
 
 snagTransMat_test <- matrix(
   c(0.48, 0.38, 0.05, 0.00, 0.00,
